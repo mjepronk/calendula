b8ba0d254323c7a8d48afd3de0395d344d3056c6
Matthias Pronk <git@masida.nl>
2020-05-15 23:57:13 +0200

    Bug fix floatingHoliday.

--- src/Calendar.hs
+++ src/Calendar.hs
@@ -9,17 +9,17 @@ import RIO.Time
 import Data.Time.Calendar.Easter (gregorianEaster)
 
 
+-- Calculate the `Day` for the `k`'th weekday in a month. If `k` is negative, we
+-- count from the end of the month.
 floatingHoliday :: Integer -> Int -> DayOfWeek -> Int -> Day
 floatingHoliday year month d k
-    | k > 0 = addDays (daysFromUntil firstDay d + weeks) firstDay
-    | k < 0 = addDays (negate (daysFromUntil lastDay d + weeks) - 1) lastDay
-    | otherwise = error "floatingHoliday: invalid k = 0"
+    | k >= 0    = addDays (daysFromUntil firstDay d + (7 * (max (k' - 1) 0))) firstDay
+    | otherwise = addDays (daysFromUntil firstDayNextMonth d - (7 * k')) firstDayNextMonth
   where
     daysFromUntil x weekday = fromIntegral $ (7 + (fromEnum weekday - fromEnum (dayOfWeek x))) `mod` 7
     firstDay = fromGregorian year month 1
-    lastDay = addDays (fromIntegral (gregorianMonthLength year month) - 1) firstDay
-    weeks = fromIntegral $ 7 * (abs k - 1)
-
+    firstDayNextMonth = addDays (fromIntegral (gregorianMonthLength year month)) firstDay
+    k' = fromIntegral (abs k)
 
 easterHoliday :: Integer -> Integer -> Day
 easterHoliday year days = addDays days (gregorianEaster year)
--- test/CalendarSpec.hs
+++ test/CalendarSpec.hs
@@ -10,7 +10,17 @@ import Calendar
 spec :: Spec
 spec = do
   describe "floatingHoliday" $ do
-    it "can calculate the first monday of january 2020" $
+    it "calculates the first weekday of the month" $
       floatingHoliday 2020 1 Monday 1 `shouldBe` fromGregorian 2020 1 6
-    it "can calculate the last monday of january 2020" $
+    it "calculates the last weekday of the month" $
       floatingHoliday 2020 1 Monday (-1) `shouldBe` fromGregorian 2020 1 27
+    it "calculates the first weekday of the month if it's the first day of the month" $
+      floatingHoliday 2020 5 Friday 1 `shouldBe` fromGregorian 2020 5 1
+    it "calculates the last weekday of the month if it's the last day of the month" $
+      floatingHoliday 2020 5 Sunday (-1) `shouldBe` fromGregorian 2020 5 31
+    it "calculates the third weekday of the month" $
+      floatingHoliday 2020 9 Tuesday 3 `shouldBe` fromGregorian 2020 9 15
+    it "calculates the first weekday of the month when k = 0" $
+      floatingHoliday 2020 1 Monday 0 `shouldBe` fromGregorian 2020 1 6
+    it "calculates the first weekday of the month if it's the first day of the month when k = 0" $
+      floatingHoliday 2020 5 Friday 0 `shouldBe` fromGregorian 2020 5 1
